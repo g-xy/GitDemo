368d12ee9802e07f27145c9c2843cd10840b3942
gxy <guanxiaoyi24@126.com>
2019-08-30 01:53:15 +0800

    c文件

---
+++ TestClass.c
@@ -0,0 +1,9 @@
+//
+//  TestClass.c
+//  GitDemo
+//
+//  Created by gxy on 2019/8/30.
+//  Copyright © 2019 gxy. All rights reserved.
+//
+
+#include "TestClass.h"
---
+++ TestClass.h
@@ -0,0 +1,14 @@
+//
+//  TestClass.h
+//  GitDemo
+//
+//  Created by gxy on 2019/8/30.
+//  Copyright © 2019 gxy. All rights reserved.
+//
+
+#ifndef TestClass_h
+#define TestClass_h
+
+#include <stdio.h>
+
+#endif /* TestClass_h */
